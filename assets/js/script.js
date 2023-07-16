dfdde9b898ac4b7b2fe9c568b4c3e357381c876f
Abrentis <brentknox@rocketmail.com>
2023-07-15 20:30:41 -0400

    Finished generator function and reorganized code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,69 +1,59 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
-var completeArray = "";
-var lowerCaseArray = "abcdefghijklmnopqrstuvwxyz".split("");
-var upperCaseArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-var numbersArray = "0123456789".split("");
-  // 32 special characters
-var specialCharactersArray = ["\`", "\~", "\!", "\@", "\#", "\$", "\%", "\^", "\&", "\*", "\(", "\)", "\-", "\_", "\=", "\+", "\[", "\{", "\]", "\}", "\;", "\:", "\'", "\"", "\\", "\|", "\,", "\<", "\.", "\>", "\/", "\?"];
-
-console.log(lowerCaseArray);
-console.log(upperCaseArray);
-console.log(numbersArray);
-console.log(specialCharactersArray);
 
 function generatePassword() {
-  // Establishing length of password
+
+  // Resets variables to empty values if a user presses the button 'Generate Password' more than once
+  var completeArray = [];
+  var passwordName = [];
+  var passwordText = document.querySelector("#password")
+  passwordText.value = "";
+
+  // Establishes length of password
   var passwordLength = prompt("How many characters would you like your password to have? (Minimum: 8 | Maximum: 128)");
   if (!passwordLength) {
     return;
   }
+      // Resets if user enters a number outside of the specified character limits
   else if (passwordLength < 8 || passwordLength > 128) {
-    var invalidLength = alert("Please enter a number from 8 to 128.");
+    alert("Please enter a number from 8 to 128.");
     generatePassword();
-  }
-  else {
-    password.length = passwordLength;
   };
 
-  // Establishing lowercase characters
-  var passwordLowerCase = confirm("Press OK to confirm including lowercase letters.");
+  // Includes lowercase characters
+  var passwordLowerCase = confirm("Press OK to confirm including lowercase letters in your password.");
   if (passwordLowerCase) {
-
-  }
-  else {
-
+    var lowerCaseArray = "abcdefghijklmnopqrstuvwxyz".split("");
+    completeArray.push(...lowerCaseArray);
   };
 
-  // Establishing uppercase characters
-  var passwordUpperCase = confirm("Press OK to confirm including uppercase letters.");
+  // Includes uppercase characters
+  var passwordUpperCase = confirm("Press OK to confirm including uppercase letters in your password.");
   if (passwordUpperCase) {
-
-  }
-  else {
-
+    var upperCaseArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    completeArray.push(...upperCaseArray);
   };
 
-  //Establishing special characters
-  var passwordSpecialCharacters = confirm("Press OK to confirm including special characters.");
+  // Includes numbers
+  var passwordNumbers = confirm("Press OK to confirm including numbers in your password.")
+  if (passwordNumbers) {
+    var numbersArray = "0123456789".split("");
+    completeArray.push(...numbersArray);
+  };
+
+  // Includes special characters
+  var passwordSpecialCharacters = confirm("Press OK to confirm including special characters in your password.");
   if (passwordSpecialCharacters) {
+    var specialCharactersArray = ["\`", "\~", "\!", "\@", "\#", "\$", "\%", "\^", "\&", "\*", "\(", "\)", "\-", "\_", "\=", "\+", "\[", "\{", "\]", "\}", "\;", "\:", "\'", "\"", "\\", "\|", "\,", "\<", "\.", "\>", "\/", "\?"];
+    completeArray.push(...specialCharactersArray);
+  };
 
-  }
-  else {
-
+  // 'For loop' that executes until the desired password length is reached
+  for (i = 0; i < passwordLength; i++) {
+    var passwordCalc = Math.floor(Math.random() * completeArray.length);
+    var passwordName = completeArray[passwordCalc];
+    var passwordText = document.querySelector("#password").value += (passwordName);
   };
 };
 
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-};
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Event listener that responds to the button being clicked, then executes 'generatePassword' function
+generateBtn.addEventListener("click", generatePassword);
